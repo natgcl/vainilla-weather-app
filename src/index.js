a952dbee464fcd7c92cd0a0f077cce8610ed0943
natgcl <80371048+natgcl@users.noreply.github.com>
2021-04-19 15:02:28 -0500

    added the search engine

--- src/index.js
+++ src/index.js
@@ -36,10 +36,25 @@ function displayTemperature (response) {
 
 }
 
-let apiKey = "005492bbca20a72f226defd2b2fda6d2"
-let city = "paris"
+
+
+function search(city){
+  let apiKey = "005492bbca20a72f226defd2b2fda6d2";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
-q=${city}&appid=${apiKey}&units=metric`;
+q=${city}&appid=${apiKey}&units=metric`; 
+axios.get(apiUrl).then(displayTemperature); 
+}
 
 
-axios.get(apiUrl).then(displayTemperature);
+
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+search("London")
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
